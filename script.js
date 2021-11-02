f201294e3f0d3688ed9daab419552de2da338f78
Tornike Maghradze <mt.georgian@gmail.com>
2021-11-02 20:11:38 +0400

    all tasks solved

--- script.js
+++ script.js
@@ -86,9 +86,9 @@
 // 3. მოცემულია მასივი: let array = [12, 25, 3, 6, 8, 14, 7, 23];
 // map-ის გამოყენებით შექმენით ახალი მასივი, რომელშიც array მასივიდან თითოეული ელემენტი იქნება 3-ზე გაყოფილი;
 
-            let array = [12, 25, 3, 6, 8, 14, 7, 23];
-            
-            let itBe = array.map( toBe => toBe / 3);
+            let array001 = [12, 25, 3, 6, 8, 14, 7, 23];
+            console.log();
+            let itBe = array001.map( toBe => toBe / 3);
             console.log(itBe);
 
 
@@ -96,31 +96,54 @@
 // filter-ის გამოყენებით გაფილთრეთ და გამოიტანეთ მარტო ციფრები; 
 
             let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
-
-            let newLet = array.filter( numbers => {
-                if (typeof numbers === 'number') {
-                    console.log(numbers);
-                }
-            });
+            console.log();
+            let newLet = array.filter( numbers => typeof numbers === 'number');
+            
             console.log(newLet);
 
-// 5.მოცემულია მასივი:
-
-// let languages = ['html', 'css', 'javascript', 'python, 'php'];
+// 5.მოცემულია მასივი: let languages = ['html', 'css', 'javascript', 'python, 'php'];
 // ფილტრის საშულებით გაფილტრეთ და გამოიტანეთ მარტო ის სიტყვები რომლების სიმბოლოების რაოდენობა იქნება 3-ზე მეტი;
 
-// 6.მოცემულია მასივი:
-// let array= ['academy', 'of', 'digital', 'industries']
+            let languages = ['html', 'css', 'javascript', 'python', 'php'];
+            console.log();
+            let itCode = languages.filter( forFun => forFun.length > 3);
 
+            console.log(itCode);
+
+
+// 6.მოცემულია მასივი: let array= ['academy', 'of', 'digital', 'industries']
 // Reduce-ის საშუალებით მიიღეთ ერთი კომბინირებული სტრინგი(academy of digital industries);
 
-// 7. მოცემულია მასივი:
-// let item = [12, ‘google’, 32, null, ‘yahoo’, 25];
+            console.log();
+            let itStepAcademy = ['academy', 'of', 'digital', 'industries'].reduce( (t,m) => t + ' ' + m);
+            console.log(itStepAcademy);
+            
 
-// შექმენით ახალი მასივი map ფუნქციის გამოყენებით შემდეგი ლოგიკით: თუ ელემენტი არის რიცხვი ამ მასივში უნდა იყოს ამ რიცხვის კვადრატი. თუ ელემენტი არის ტექსტი, მაშინ ახალ მასივში უნდა იყოს ეს ტექსტი დიდი ასოებით. სხვა შემთხვევაში ელემენტები უნდა დაემატოს უცვლელად;
+// 7. მოცემულია მასივი: let item = [12, ‘google’, 32, null, ‘yahoo’, 25];
+//  შექმენით ახალი მასივი map ფუნქციის გამოყენებით შემდეგი ლოგიკით: თუ ელემენტი არის რიცხვი ამ მასივში უნდა იყოს ამ რიცხვის კვადრატი. 
+// თუ ელემენტი არის ტექსტი, მაშინ ახალ მასივში უნდა იყოს ეს ტექსტი დიდი ასოებით. სხვა შემთხვევაში ელემენტები უნდა დაემატოს უცვლელად;
+
+            console.log();
+            let item = [12, 'google', 32, null, 'yahoo', 25];
+            let itSnow = item.map( snow => {
+                if (typeof snow === 'number'){
+                    return snow * snow;
+                } else if (typeof snow === 'string' ) {
+                    return snow.toLocaleUpperCase();
+                } else {
+                    return snow
+                }
+            });
+
+            console.log(itSnow);
+            console.log();
 
 
-// 8. Მოცემულია მასივი:
-// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
 
+// 8. Მოცემულია მასივი: let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
 // Filter მეთოდის საშუალებით დააბრუნეთ მასივი, რომელიც შედგება ისეთი სიტყვებისგან რომელიც შეიცავს ასო m-ს ან M-ს;
+            
+            let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+            let itWin = words.filter( justM => justM.includes ('M') || justM.includes('m'));
+
+            console.log(itWin);
